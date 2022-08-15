1b8d69b571a1c2fb1bf199360c936ac2f2f1822e
lucflores <luciaflores0609@gmail.com>
2022-08-14 22:27:01 -0300

    Login and Register and Navbar

--- front/src/componentes/Login/Login.js
+++ front/src/componentes/Login/Login.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './Login.css';
 
 const Login = () => {
-  return (
-    <div>
-        <h1>Login1</h1>
-    </div>
-  )
-}
+	const [inputPassword, cambiarInputPassword] = useState('');
+	const [inputCorreo, cambiarInputCorreo] = useState('');
 
-export default Login
+	// Funcion que se encargara de validar los datos y enviar el formulario
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Comprobamos validacion del formulario ...
+		// Si todo es correcto enviamos el formulario
+
+		console.log('Formulario Enviado!');
+	}
+
+	// Funcion que se encarga de cambiar el estado del inputNombre
+	const handleInputPassword = (e) => {
+		cambiarInputPassword(e.target.value);
+	}
+	
+	// Funcion que se encarga de cambiar el estado del inputCorreo
+	const handleInputCorreo = (e) => {
+		cambiarInputCorreo(e.target.value);
+	}
+
+	return (
+		<>
+			
+			<form action="" onSubmit={handleSubmit} className="login">
+				<div>
+					<label htmlFor="correo">Correo</label>
+					<input
+						type="email"
+						name="correo"
+						placeholder="Correo@outlook.com"
+						id="correo"
+						value={inputCorreo}
+						onChange={handleInputCorreo}
+					/>
+				</div>
+        		<div>
+					<label htmlFor="password">Clave</label>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						id="password"
+						value={inputPassword}
+						onChange={handleInputPassword}
+					/>
+				</div>
+
+				<button type="submit" href="/Home">Ingresar</button>
+			</form>
+		</>
+	);
+}
+ 
+export default Login;
