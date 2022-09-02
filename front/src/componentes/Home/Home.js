5df8f54a0e0cf828db144419edf413d66e5c5ac8
lucflores <luciaflores0609@gmail.com>
2022-09-02 14:35:51 -0300

    arreglo de home

--- front/src/componentes/Home/Home.js
+++ front/src/componentes/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Home.css"
-import logo from '../Imagenes/logo.png';
+
 import { Link } from 'react-router-dom';
 
 
@@ -8,7 +8,7 @@ export const Home = () => {
   return (
     <div>
         
-        <img src= {logo} alt='logo' className='Container' />
+        <img src= '../Imagenes/logo.png' alt='logo' className='Container' />
         
 
           <Link to="../principal">
